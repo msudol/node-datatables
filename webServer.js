7c96e42f3c8946700422041bb7ae38b868035329
msudol <msudol@anaren.com>
2018-06-04 12:22:36 -0400

    basic auth with session, nedb session store

--- webServer.js
+++ webServer.js
@@ -6,8 +6,17 @@
 */
 "use strict";
 
+// trying out some session storage
+var sharedSecretKey = 'simplesecret';
+// let's get sessions now
+var session = require('express-session');
+// can't have a webserver without express
 var express = require('express');
 var app = express();
+
+// connect the NedbStore to session
+var NedbStore = require('nedb-session-store')(session);
+
 var router = express.Router();
 var Api = require('./routes/api');
 // yes it's a class for the webserver, why? I don't know yet but we'll see.
@@ -21,9 +30,64 @@ class WebServer {
         
         this.api = new Api(this.db);
         
-        // serve static files from the public folder at /
-        this.app.use(express.static('public'));
+        this.app.use(
+            session({
+                secret: sharedSecretKey,
+                resave: false,
+                saveUninitialized: false,
+                cookie: {
+                    path: '/',
+                    httpOnly: true,
+                    maxAge: 365 * 24 * 60 * 60 * 1000   // e.g. 1 year
+                },
+                store: new NedbStore({
+                    filename: 'sess/nedb_persistence_file.db'
+                })
+            })
+        );
    
+        /*** TESTING AUTH - move to a submodule at some point ***/
+        
+        // Authentication and Authorization Middleware
+        var auth = function(req, res, next) {
+            console.log("Checking Authorization");
+            if (req.session && req.session.user === "user" && req.session.admin) {
+                return next();
+            } else {
+                return res.sendStatus(401);
+            }
+        };
+
+        // Login endpoint - uses a get query at the moment - http://localhost:3000/login/?username=user&password=password
+        app.get('/login', function (req, res) {
+          if (!req.query.username || !req.query.password) {
+            res.send('login failed');    
+          } else if (req.query.username === "user" || req.query.password === "password") {
+              // this is ugly and will have to be better
+            req.session.user = "user";
+            req.session.admin = true;
+            res.send("login success!");
+          }
+        });
+
+        // Logout endpoint
+        app.get('/logout', function (req, res) {
+          req.session.destroy();
+          res.send("logout success!");
+        });
+
+        
+        // serve static files from the public folder at /   
+        this.app.use(express.static('public'));      
+        
+        // Get content endpoint
+        app.use( "/private", [ auth, express.static( __dirname + "/private" ) ] );      
+        
+        
+        /*** END AUTH TEST ***/
+        
+        
+        
         // the api route handler
         this.app.use('/api', this.api.handler); 
         
